6113f7d2d784281f27d86c968855cad937d974e2
UTSWEB <ayuchandradewi240@gmail.com>
2021-06-21 16:04:54 +0800

    progres uas

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+    <div class="jumbotorn text-center">
+    <img src="smb.jpg" width="400">
+
+    <div class="text-left">
+      <h3>About Us</h3>
+      <h6>Jimshoney merupakan salah satu brand lokal yang ada di Indonesia. Jimshoney menawarkan produk berkualitas, mulai dari tas, dompet, hingga jam tangan untuk pria maupun wanita.</h6>
+      <h6>Produk Jimshoney dibuat menggunakan bahan kulit dengan kualitas terbaik dan tentunya dengan harga yang tidak terlalu menguras kantong.</h6>
+      <h6>Tas casual wanita lebih dari 40 model, dompet model casual seperti dompet kartu, dompet handphone, dompet two in one, dompet kecil dan dompet koin lebih dari 30 model.</h6>
+      <h6>Kemudian jam tangan yang juga memiliki lebih dari 30 model.</h6>
+      <h6>Produk Jimshoney cocok untuk wanita disegala aktivitas baik formal atau santai.</h6>
+    </div>
+    
+</x-app-layout>
